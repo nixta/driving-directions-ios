980f8191fe984fdf82b8a2c8ba6875efb7df876d
Al Pascual <alpascual@gmail.com>
2012-10-01 12:27:34 -0700

    adding the nearby screen

---
+++ Map/Nearby/NearByViewController.h
@@ -0,0 +1,15 @@
+//
+//  NearByViewController.h
+//  Map
+//
+//  Created by Al Pascual on 10/1/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NearByViewController : UIViewController
+
+@property (nonatomic,strong) IBOutlet UIWebView *webView;
+
+@end
---
+++ build/Map.build/Debug-iphoneos/Map-Source.build/Objects-normal/armv7/NearByViewController.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/albe5259/Projects/navigator/Map/Nearby/NearByViewController.m \
+  /Users/albe5259/Projects/navigator/Map/Nearby/NearByViewController.h
