e5896bc6ed5a0993d660283c17300b4d13e7f4c3
Al Pascual <alpascual@gmail.com>
2012-10-12 13:29:37 -0700

    Sync untrack files

--- build/Map.build/Debug-iphoneos/Map-Source.build/Objects-normal/armv7/ExtendableToolbar.d
+++
@@ -1,3 +0,0 @@
-dependencies: /Users/albe5259/Projects/navigator/Map/ExtendableToolbar.m \
-  /Users/albe5259/Projects/navigator/Map/ExtendableToolbar.h \
-  /Users/albe5259/Projects/navigator/Map/PassThroughView.h
